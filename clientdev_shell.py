951dd480cacad8c3e9c90ef2d921af0540c07981
Lionel Ains <lionel.ains@legrand.fr>
2015-03-16 21:22:51 +0100

    Adding commands set_tunnelling_dev_uplink_type, wait_vtun_allowed and get_vtun_parameters to client shell

--- clientdev_shell.py
+++ clientdev_shell.py
@@ -6,19 +6,24 @@ from __future__ import print_function
 
 import cmd
 import os
+import os.path
 import sys
 import re
 import ipaddr
+import time
 
 import tundev_shell
 
 class ClientDevShell(tundev_shell.TunnellingDevShell):
     """ Tundev CLI shell offered to client dev """
 
+    VTUN_READY_FNAME_PREFIX = "/var/run/vtun_ready-"
+    
     def __init__(self, username):
         tundev_shell.TunnellingDevShell.__init__(self, username)
         self.lan_ip_address = None
         self.lan_ip_prefix = None
+        self.uplink_type = None
 
     # Only for support
     #~ def do_set_tunnel_mode(self, args):
@@ -32,13 +37,48 @@ class ClientDevShell(tundev_shell.TunnellingDevShell):
 
 Publish the LAN IP address of the tunnelling dev
 Argument address should contain the IP address and the CIDR prefix separated by a character '/'
-eg: 192.168.1.2/24"""
+eg: "192.168.1.2/24\""""
         try:
             ipv4=ipaddr.IPv4Network(args)
             self.lan_ip_address = ipv4.ip
             self.lan_ip_prefix = ipv4._prefixlen
         except ValueError:
             print('Invalid IP network: ' + args, file=sys.stderr)
+
+    def do_set_tunnelling_dev_uplink_type(self, args):
+        """Usage: set_tunnelling_dev_uplink_type {type}
+
+Publish the type of uplink used by the tunnelling dev
+Argument type is a string
+eg: "lan\""""
+        if args == 'lan' or args == '3g':
+            self.uplink_type = args
+        else:
+            print('Unsupported uplink type: ' + args, file=sys.stderr)
+
+    def do_wait_vtun_allowed(self, args):
+        """Usage: wait_vtun_allowed
+
+Wait until the RDV server is ready to accept a new vtun session.
+
+Output the readiness status of the RDV server, possible return values are "ready", "not_ready"
+"""
+        timeout = 60    # 60s
+        vtun_check_fname = ClientDevShell.VTUN_READY_FNAME_PREFIX + self._username
+        print('Checking "%s"' % (vtun_check_fname))
+        while timeout>0:
+            if os.path.isfile(vtun_check_fname):
+                print('ready')
+                return False
+            else:
+                time.sleep(1)
+                timeout -= 1
+        print('not_ready', file=sys.stderr)
+    
+    def do_get_vtun_parameters(self, args):
+        self._generate_vtun_config()
+        self._start_vtun()
+        print(self._vtun_config_to_str())
 
 if __name__ == '__main__':
     username = str(os.getuid())
--- tundev_shell.py
+++ tundev_shell.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 import cmd
 import os
 import sys
+import ipaddr
 
 class TunnellingDevShell(cmd.Cmd):
     """ Tundev CLI shell offered to tunnelling devices """
@@ -16,7 +17,12 @@ class TunnellingDevShell(cmd.Cmd):
         self.tunnel_mode = 'L3'
         self._username = shell_user_name
         self.prompt = self._username + '$ '
-
+        
+        self._vtun_tunnel_ip_network = None
+        self._vtun_tunnelling_dev_ip = None
+        self._vtun_rdv_server_ip = None
+        self._vtun_rdv_server_tcp_port = None
+    
     #~ def do_connect(self, args):
         #~ """Connect to all hosts in the hosts list"""
         #~ self._ssh_connection = paramiko.SSHClient()
@@ -66,3 +72,40 @@ Terminates this command-line session"""
     def do_EOF(self, args):
         """Send EOF (^D) to terminates this command-line session"""
         return self.do_exit(args)
+
+    def _generate_vtun_config(self):
+        """ Populate the attribute related to the vtun tunnel of this object """
+        if self._username == '1000':    # For our (only) client RPI
+            self._vtun_tunnel_ip_network = ipaddr.IPv4Network('192.168.100.0/30')
+            self._vtun_tunnelling_dev_ip = ipaddr.IPv4Address('192.168.100.2')
+            self._vtun_rdv_server_ip = ipaddr.IPv4Address('192.168.100.1')
+            self._vtun_rdv_server_tcp_port = 5000
+        elif self._username == '1001':    # For our (only) support RPI
+            self._vtun_tunnel_ip_network = ipaddr.IPv4Address('192.168.101.0/30')
+            self._vtun_tunnelling_dev_ip = ipaddr.IPv4Address('192.168.101.2')
+            self._vtun_rdv_server_ip = ipaddr.IPv4Address('192.168.101.1')
+            self._vtun_rdv_server_tcp_port = 5001
+        else:
+            print('Unknown tunnelling device account "%s"... cannot generate vtun parameters' % (self._username), file=sys.stderr)
+
+    def _has_valid_vtun_config(self):
+        if self._vtun_tunnel_ip_network is None or self._vtun_tunnelling_dev_ip is None or self._vtun_rdv_server_ip is None or self._vtun_rdv_server_tcp_port is None:
+            return False
+        return True
+
+    def _start_vtun(self):
+        """ Start the vtun service according to the remote tundev shell configuration """
+        if not self._has_valid_vtun_config():
+            print('Cannot start rdv server side vtun. vtun is not fully configured', file=sys.stderr)
+        else:
+            pass
+    
+    def _vtun_config_to_str(self):
+        message = ''
+        message += 'tunnel_ip_network: ' + str(self._vtun_tunnel_ip_network.network) + '\n'
+        message += 'tunnel_ip_prefix: /' + str(self._vtun_tunnel_ip_network.prefixlen) + '\n'
+        message += 'tunnel_ip_netmask: ' + str(self._vtun_tunnel_ip_network.netmask) + '\n'
+        message += 'tunnelling_dev_ip_address: ' + str(self._vtun_tunnelling_dev_ip) + '\n'
+        message += 'rdv_server_ip_address: ' + str(self._vtun_rdv_server_ip) + '\n'
+        message += 'rdv_server_vtun_tcp_port: ' + str(self._vtun_rdv_server_tcp_port)
+        return message
